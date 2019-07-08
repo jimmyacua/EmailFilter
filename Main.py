9187043663126ed114a2612a265105a8a6cc737d
jimmyacua <jimmyacua@hotmail.com>
2019-07-07 21:06:42 -0600

    first commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+
+emails = pd.read_csv('data/Emails.csv')
+print(emails.ExtractedBodyText) #(7945 emails, 22 columns)
+
+
